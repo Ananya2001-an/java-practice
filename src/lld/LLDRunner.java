65c82233b4968633c9c51190d9c7b1d3f660c4c3
Ananya Nayak <ananyanayak102@gmail.com>
2024-09-17 20:50:12 +0530

    LRU cache LLD

---
+++ src/lld/LLDRunner.java
@@ -0,0 +1,8 @@
+import LRUCache.LRUCacheDemo;
+
+public class LLDRunner{
+    public static void main(String[] args){
+        // call static methods directly, no need to create class instance
+        LRUCacheDemo.run();
+    }
+}
